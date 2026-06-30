c873dc0635a12ba441752a4fb7653cf8fa2a7cc5
MattWate <matthew@dubyahinnovation.com>
2026-06-30 19:53:34 +0200

    Mount modular app structure

--- src/persistent-mount.js
+++ src/persistent-mount.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.product.jsx';
-import './styles.css';
+import App from './app/App.jsx';
 
 const root = document.querySelector('#root');
 createRoot(root).render(React.createElement(React.StrictMode, {}, React.createElement(App)));
