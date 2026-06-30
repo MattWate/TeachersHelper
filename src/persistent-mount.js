2bd3a00bdfbcd63630acce88ab8f04649884030e
MattWate <matthew@dubyahinnovation.com>
2026-06-30 18:41:27 +0200

    Use guided onboarding app

--- src/persistent-mount.js
+++ src/persistent-mount.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.product.jsx';
+import App from './App.onboarding.jsx';
 import './styles.css';
 
 createRoot(document.querySelector('#root')).render(React.createElement(React.StrictMode, {}, React.createElement(App)));
