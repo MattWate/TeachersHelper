17a03ea1e90fc3c6a76679af7d2a5d59434473cf
MattWate <matthew@dubyahinnovation.com>
2026-06-30 18:43:41 +0200

    Use product app entry

--- src/persistent-mount.js
+++ src/persistent-mount.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.onboarding.jsx';
+import App from './App.product.jsx';
 import './styles.css';
 
-createRoot(document.querySelector('#root')).render(React.createElement(React.StrictMode, {}, React.createElement(App)));
+const root = document.querySelector('#root');
+createRoot(root).render(React.createElement(React.StrictMode, {}, React.createElement(App)));
